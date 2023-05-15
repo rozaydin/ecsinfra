e0c8f35871da34ad7e4aa6171d944f7fe6ed5ce0
rozaydin <rozaydin@gmail.com>
2023-05-15 18:03:50 +0300

    sample ecs backend
    Signed-off-by: rozaydin <rozaydin@gmail.com>

--- main.tf
+++ main.tf
@@ -6,30 +6,30 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "test-ecs-vpc"
+  name = "apollo"
   cidr = "10.58.0.0/16"
 
   azs             = ["eu-central-1a", "eu-central-1b"]  
   public_subnets  = ["10.58.1.0/24", "10.58.2.0/24"]
+  private_subnets = ["10.58.3.0/24", "10.58.4.0/24"]
 
-  enable_nat_gateway  = false  
+  enable_nat_gateway  = true
   tags = var.tags
 }
 
 # ECS Cluster Definition
 
-
 ## Security Group for ECS Cluster
 
-resource "aws_security_group" "allow_http" {
+resource "aws_security_group" "allow_https" {
   
-  name = "allow_http"
+  name = "allow_https"
   depends_on = [ module.vpc ]
-  description = "Allow HTTP inbound traffic"
+  description = "Allow HTTPS inbound traffic"
   vpc_id = module.vpc.vpc_id
 
    ingress {
-    description      = "HTTP from VPC"
+    description      = "HTTP from anywhere"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
@@ -44,9 +44,7 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
 }
-
 
 resource "aws_security_group" "ecs_tasks" {
   name        = "ecs-tasks-sg"
@@ -57,8 +55,8 @@ resource "aws_security_group" "ecs_tasks" {
     protocol        = "tcp"
     from_port       = 80
     to_port         = 80    
-    security_groups = [aws_security_group.allow_http.id]
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.allow_https.id]
+    # cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -66,23 +64,6 @@ resource "aws_security_group" "ecs_tasks" {
     from_port   = 0
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_ecs_cluster" "foo" {
-  name = "foo"  
-}
-
-resource "aws_ecs_cluster_capacity_providers" "foo_capacity_providers" {
-  cluster_name = aws_ecs_cluster.foo.name
-
-  capacity_providers = ["FARGATE"]
-
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE"
   }
 }
 
@@ -103,17 +84,16 @@ output "vpc_public_subnets" {
   value = module.vpc.public_subnets
 }
 
-
 resource "aws_lb" "ecs_lb" {
   name               = "alb"
   subnets            = module.vpc.public_subnets
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_http.id]
+  security_groups    = [aws_security_group.allow_https.id]
 
   tags = var.tags
 }
 
-resource "aws_lb_listener" "https_forward" {
+resource "aws_lb_listener" "http_forward" {
   load_balancer_arn = aws_lb.ecs_lb.arn
   port              = 80
   protocol          = "HTTP"
@@ -123,6 +103,13 @@ resource "aws_lb_listener" "https_forward" {
     target_group_arn = aws_lb_target_group.ecs_alb_tg.arn
   }
 }
+
+# resource "aws_acm_certificate" "example" { }
+
+# resource "aws_lb_listener_certificate" "" {
+#   listener_arn    = aws_lb_listener.https_forward.arn
+#   certificate_arn = aws_acm_certificate.apollodev.arn
+# }
 
 resource "aws_lb_target_group" "ecs_alb_tg" {
   name        = "ecs-alb-tg"
@@ -142,25 +129,41 @@ resource "aws_lb_target_group" "ecs_alb_tg" {
   # }
 }
 
-resource "aws_ecs_service" "foo_service" {
+resource "aws_ecs_cluster" "apollo_ecs_cluster" {
+  name = "apollo-backend"  
+}
+
+resource "aws_ecs_cluster_capacity_providers" "foo_capacity_providers" {
+  cluster_name = aws_ecs_cluster.apollo_ecs_cluster.name
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
+}
+
+resource "aws_ecs_service" "apollo_backend_ecs_service" {
 
   depends_on = [
-    aws_ecs_task_definition.foo_task_definition,
+    aws_ecs_task_definition.apollo_backend_task_definition,
     aws_lb_target_group.ecs_alb_tg
   ]
 
-  name            = "foo"
-  cluster         = aws_ecs_cluster.foo.id
-  task_definition = aws_ecs_task_definition.foo_task_definition.arn
+  name            = "apollo-backend-ecs-service"
+  cluster         = aws_ecs_cluster.apollo_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.apollo_backend_task_definition.arn
   desired_count   = 1  
   # iam_role        = data.aws_iam_role.ecs_service_role.arn
 
   launch_type = "FARGATE"
 
   network_configuration {
-    subnets = module.vpc.public_subnets
+    subnets = module.vpc.private_subnets
     security_groups = [aws_security_group.ecs_tasks.id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
 
   load_balancer {
@@ -171,8 +174,31 @@ resource "aws_ecs_service" "foo_service" {
   
 }
 
-resource "aws_ecs_task_definition" "foo_task_definition" {
-  family = "foo_service"
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+  tags = var.tags
+}
+
+resource "aws_ecs_task_definition" "apollo_backend_task_definition" {
+  family = "apollo_backend_service"
   container_definitions = jsonencode([
     {
       name      = "nginx"
@@ -199,7 +225,7 @@ resource "aws_ecs_task_definition" "foo_task_definition" {
   ])
 
   # task_role_arn = "arn:aws:iam::221148627084:role/ecsTaskFullAccess"
-  execution_role_arn = "arn:aws:iam::221148627084:role/ecsTaskExecutionRole"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   requires_compatibilities = [ "FARGATE" ]
   cpu = "1024"
